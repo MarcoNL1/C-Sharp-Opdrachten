faed9dc589e94321d7c7c8f92ab0fe84aef5235e
murrok <marco.speksnijder@gmail.com>
2023-02-11 17:26:55 +0100

    Opdracht H03 Dag 2

---
+++ W01.2.H03/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Enter a starting number: ");
+int startNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter an end number: ");
+var endNumber = Convert.ToInt32(Console.ReadLine());
+
+for (var number = startNumber; number <= endNumber; number++)
+{
+    if (number % 3 == 0 & number % 5 != 0)
+    {
+        Console.WriteLine("Fizz");
+        continue;
+    }
+
+    if (number % 5 == 0 & number % 3 != 0)
+    {
+        Console.WriteLine("Buzz");
+        continue;
+    }
+
+    if (number % 3 == 0 & number % 5 == 0)
+    {
+        Console.WriteLine("Fizzbuzz");
+    }
+
+    else
+    {
+        Console.WriteLine(number);
+    }
+}
