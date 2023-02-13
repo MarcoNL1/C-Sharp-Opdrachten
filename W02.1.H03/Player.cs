3c6e17f02f8b555cca514aa9663a936fd2ae71c9
murrok <marco.speksnijder@gmail.com>
2023-02-13 16:25:45 +0100

    Add Homework projects

---
+++ W02.1.H03/Player.cs
@@ -0,0 +1,15 @@
+﻿namespace W02._1.H03;
+
+public class Player
+{
+    public string Name;
+    public int HealthPoints;
+    public int Power;
+
+    public Player(string name, int healthPoints, int power)
+    {
+        Name = name;
+        HealthPoints = healthPoints;
+        Power = power;
+    }
+}
