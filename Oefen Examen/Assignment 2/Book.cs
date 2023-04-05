a32858becb68fd69de55ea4a9ee7b42d3b7b4c5b
murrok <marco.speksnijder@gmail.com>
2023-04-05 11:04:43 +0200

    Added oefen examen

---
+++ Oefen Examen/Assignment 2/Book.cs
@@ -0,0 +1,18 @@
+﻿public class Book{
+    public readonly string Title;
+    public readonly string Author;
+    protected string Language;
+
+    public Book(string title, string author, string language){
+        Title = title;
+        Author = author;
+        Language = language;
+        Title = title;
+        Author = author;
+        Language = "EN";
+    }
+
+    public virtual void PrintInfo(){
+        Console.WriteLine($"Title: {Title}\n Author: {Author}\n Language: {Language}");
+    }
+}
